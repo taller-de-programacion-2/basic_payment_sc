41044ceb49df6d98ed1cf32fb63238fd9b1862c3
jowisaidman <joelsaidman1@gmail.com>
2022-06-26 18:28:31 -0300

    feat: change statu_code response for deposits

--- src/handlers/createDepositHandler.js
+++ src/handlers/createDepositHandler.js
@@ -16,9 +16,10 @@ function schema() {
 }
 
 function handler({ contractInteraction, walletService }) {
-  return async function (req) {
+  return async function (req, reply) {
     const senderWallet = await walletService.getWallet(req.body.senderId);
-    return contractInteraction.deposit(senderWallet, req.body.amountInEthers, req.body.senderId);
+    const body = await contractInteraction.deposit(senderWallet, req.body.amountInEthers, req.body.senderId);
+    return reply.code(201).send(body);    
   };
 }
 
