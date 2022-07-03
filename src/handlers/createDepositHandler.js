b1cf43668e97affdefcd9c3e34b6b8c02686752e
jowisaidman <joelsaidman1@gmail.com>
2022-07-03 13:05:53 -0300

    fix: condition when creating deposit change

--- src/handlers/createDepositHandler.js
+++ src/handlers/createDepositHandler.js
@@ -19,7 +19,7 @@ function handler({ contractInteraction, walletService }) {
   return async function (req, reply) {
     const senderWallet = await walletService.getWallet(req.body.senderId);
     const body = await contractInteraction.deposit(senderWallet, req.body.amountInEthers, req.body.senderId);
-    if (body.error !== null) {
+    if (body.hash === null) {
       return reply.code(400).send({ error: body.error });
     }
     return reply.code(201).send(body);
