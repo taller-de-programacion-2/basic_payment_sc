0a5b0a3d2313b9472059c7a5efc68d9d27e67bb8
Facundo Pareja <facundopareja@gmail.com>
2023-06-03 13:55:12 -0300

    added Dockerfile

--- src/handlers/createDepositHandler.js
+++ src/handlers/createDepositHandler.js
@@ -23,7 +23,10 @@ function handler({ contractInteraction, walletService }) {
     let deployerWallet = await walletService.getDeployerWallet();
     let tx1 = await contractInteraction.deposit(walletService.getWallet(req.body.senderKey), req.body.amountInEthers);
     let tx2 = await contractInteraction.payout(walletService.getWallet(req.body.receiverKey),req.body.amountInEthers, deployerWallet)
-    return tx2
+    let jsons = [];
+    jsons.push(tx1);
+    jsons.push(tx2);
+    return jsons
   };
 }
 
