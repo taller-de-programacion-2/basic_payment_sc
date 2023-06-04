c5cb80500da5dabc0b98528842bc7d23fadccfb6
MarianoCinalli <cinallimariano1992@gmail.com>
2023-06-03 23:15:59 -0300

    chore: add sentry

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 const config = require("./config");
 const services = require("./services/services")({ config });
 const routes = require("./routes");
+const Sentry = require("@sentry/node");
+
+if (process.env.SENTRY_ENABLED) {
+  Sentry.init({dsn: process.env.SENTRY_DSN, tracesSampleRate: 1.0});
+}
 
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+
+fastify.setErrorHandler(async (error, request, reply) => {
+  // Logging locally
+  console.log(error);
+  // Sending error to be logged in Sentry
+  if (process.env.SENTRY_ENABLED) {
+    Sentry.captureException(error);
+  }
+  reply.status(500).send({ error: "Something went wrong" });
+})
 
 // Declares routes
 routes.forEach(route => fastify.route(route({ config, services })));
